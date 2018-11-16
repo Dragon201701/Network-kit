890196009e40d674b17b25f6bbaba515be6e3850
Dragon201701 <liualbert1@gmail.com>
2018-11-16 13:21:51 -0600

    update on argument

--- TCP_Client_Java/src/TCPclientJavamain.java
+++ TCP_Client_Java/src/TCPclientJavamain.java
@@ -14,8 +14,9 @@ import java.io.*;
 import java.util.*;
 
 public class TCPclientJavamain {
-	public static void start() throws UnknownHostException, IOException{
-        Socket client = new Socket("144.92.48.171", 80);
+	public static void start(int port, String ip) throws UnknownHostException, IOException{
+		System.out.println("Initializing socket server at 144.92.48.171:" + port);
+        Socket client = new Socket(ip, port);
         String modifiedSentence = null;
         try {
             PrintWriter output = new PrintWriter(client.getOutputStream(), true);
@@ -39,7 +40,7 @@ public class TCPclientJavamain {
                 		System.out.println("Received from server: " + modifiedSentence);
                 	}
             	}
-            	else if(cin.hasNextLine()) {
+            	else if(cin.hasNext()) {
             		words = cin.nextLine();
             		output.println(words);
                     System.out.println("Sending:  "+ words);
@@ -53,9 +54,23 @@ public class TCPclientJavamain {
         }
     }
 	public static void main(String[] args) {
-		System.out.println("Initializing socket server...");
+		int port = 0;
+		String ip = "144.92.48.171";
+		Scanner scnr = new Scanner(System.in);
+		if(args.length != 2) {
+			System.out.print("Enter the ip address: ");
+			ip = scnr.nextLine();
+			System.out.print("Enter the port number: ");
+			port = scnr.nextInt();
+		}
+		else {
+			ip = args[0];
+			port = Integer.parseInt(args[1]);
+		}
+		
+		
 		try {
-			start();
+			start(port, ip);
 		}catch(IOException ioe) {
 			System.out.println("Socket I/O Exception occurs. Exception message: " + ioe.getMessage());
 		}
